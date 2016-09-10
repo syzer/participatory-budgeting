cdf3a72c89e262d79288c8343604598c2dca683b
syzer3@gmail.com <syzer3@gmail.com>
2016-09-10 13:44:04 +0200

    editor config

--- index.ios.js
+++ index.ios.js
@@ -4,13 +4,13 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
 class ParticipatoryBudgeting extends Component {
   render() {
@@ -27,7 +27,7 @@ class ParticipatoryBudgeting extends Component {
           Cmd+D or shake for dev menu
         </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -48,6 +48,6 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
 
-AppRegistry.registerComponent('ParticipatoryBudgeting', () => ParticipatoryBudgeting);
+AppRegistry.registerComponent('ParticipatoryBudgeting', () => ParticipatoryBudgeting)
