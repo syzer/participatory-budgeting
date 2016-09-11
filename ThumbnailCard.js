3b706ebff08b9a8ad86dee151c6692763b62888e
syzer3@gmail.com <syzer3@gmail.com>
2016-09-11 23:55:46 +0200

    missing package, bold text in small thumbnauls

--- ThumbnailCard.js
+++ ThumbnailCard.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {
     Image,
     StyleSheet,
     Text,
     View
-} from 'react-native';
+} from 'react-native'
 
 const thumbnailStyles = StyleSheet.create({
     container: {
@@ -16,22 +16,28 @@ const thumbnailStyles = StyleSheet.create({
         height: 80,
         width: 100,
         alignSelf: 'center'
+    },
+    shortTitle: {
+        fontWeight: 'bold',
+        textAlign: 'center'
     }
-});
+})
 
 class ThumbnailCard extends Component {
     render() {
-        const image = this.props.badImage ? this.props.image : this.props.imageGood;
-        const containerStyle = this.props.containerStyle ? this.props.containerStyle : thumbnailStyles.container;
-        const imageStyle = this.props.imageStyle ? this.props.imageStyle : thumbnailStyles.image;
+        const image = this.props.badImage ? this.props.image : this.props.imageGood
+        const containerStyle = this.props.containerStyle ? this.props.containerStyle : thumbnailStyles.container
+        const imageStyle = this.props.imageStyle ? this.props.imageStyle : thumbnailStyles.image
         return (
             <View style={containerStyle}>
-                <Text numberOfLines={1} adjustsFontSizeToFit={true}>{this.props.shortTitle}</Text>
+                <Text numberOfLines={1} adjustsFontSizeToFit={true} style={thumbnailStyles.shortTitle}>
+                    {this.props.shortTitle}
+                </Text>
                 <Image
                     style={imageStyle}
                     source={image}/>
             </View>
-        );
+        )
     }
 }
-export default ThumbnailCard;
+export default ThumbnailCard
